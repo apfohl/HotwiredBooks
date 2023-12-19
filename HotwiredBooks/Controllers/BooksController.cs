44947bcdacc12246e86a80cc2d52425d7a505afb
Andreas Pfohl <mail@apfohl.com>
2023-12-19 15:03:05 +0100

    Implemented updating books

--- HotwiredBooks/Controllers/BooksController.cs
+++ HotwiredBooks/Controllers/BooksController.cs
@@ -1,5 +1,6 @@
 using HotwiredBooks.Attributes;
 using HotwiredBooks.Components;
+using HotwiredBooks.Extensions;
 using HotwiredBooks.Models;
 using HotwiredBooks.ViewModels;
 using Microsoft.AspNetCore.Mvc;
@@ -7,49 +8,42 @@ using MonadicBits;
 
 namespace HotwiredBooks.Controllers;
 
-using static Functional;
-
 public sealed class BooksController(IBooksRepository booksRepository) : Controller
 {
     private record FormData(string Title, string Author);
 
     [HttpGet]
-    public async Task<ActionResult> Index() => View(new BooksIndexViewModel(
+    public async Task<IActionResult> Index() => View(new BooksIndexViewModel(
         (await booksRepository.All()).OrderBy(book => book.Title)));
 
     [HttpGet]
-    public ActionResult New() => View();
+    public IActionResult New() => View();
 
     [HttpPost]
     [ValidateAntiForgeryToken]
     [TurboStreamResponse]
-    public async Task<ActionResult> Create(IFormCollection collection)
-    {
-        var book = await (await ParseFormData(collection))
-            .Match(
-                formData => booksRepository.Create(formData.Title, formData.Author),
-                () => Task.FromResult<Maybe<Book>>(Nothing)
-            );
-
-        return View(new BooksCreateViewModel(
-            book.Match(b => b, () => throw new ArgumentException()),
-            (await booksRepository.All()).Count())
-        );
-    }
+    public Task<IActionResult> Create(IFormCollection collection) =>
+        (
+            from formData in ParseFormData(collection)
+            from book in booksRepository.Create(formData.Title, formData.Author)
+            select book
+        )
+        .MapAsync<Book, IActionResult>(async book =>
+            View(new BooksCreateViewModel(book, (await booksRepository.All()).Count())))
+        .OrElse(StatusCode(500, "An unexpected error occurred on the server."));
 
     [HttpGet]
-    public async Task<ActionResult> Edit(Guid id) =>
-        (await booksRepository.Lookup(id))
-        .Match(
-            book => View(new BooksEditViewModel(book)),
-            () => throw new ArgumentException()
-        );
+    public Task<IActionResult> Edit(Guid id) =>
+        booksRepository
+            .Lookup(id)
+            .MapAsync<Book, IActionResult>(book => View(new BooksEditViewModel(book)))
+            .OrElse(StatusCode(500, "An unexpected error occurred on the server."));
 
     [HttpPatch, HttpPut]
     [ValidateAntiForgeryToken]
-    public async Task<ActionResult> Update(Guid id, IFormCollection collection)
-    {
-        var saveBook =
+    [TurboStreamResponse]
+    public Task<IActionResult> Update(Guid id, IFormCollection collection) =>
+        (
             from formData in ParseFormData(collection)
             from book in booksRepository.Lookup(id)
             from updatedBook in booksRepository.Update(
@@ -59,34 +53,26 @@ public sealed class BooksController(IBooksRepository booksRepository) : Controll
                     Author = formData.Author
                 }
             )
-            select updatedBook;
-
-        var savedBook = await saveBook;
-
-        return Created();
-    }
+            select updatedBook
+        )
+        .MapAsync<Book, IActionResult>(book => View(new BooksEditViewModel(book)))
+        .OrElse(StatusCode(500, "An unexpected error occurred on the server."));
 
     [HttpPost]
     [ValidateAntiForgeryToken]
     [TurboStreamResponse]
-    public async Task<ActionResult> Delete(Guid id) =>
-        await (await booksRepository
-                .Lookup(id)
-                .BindAsync(booksRepository.Delete))
-            .Match(
-                async book => View(
-                    new BooksDeleteViewModel(
-                        book,
-                        (await booksRepository.All()).Count()
-                    )
-                ),
-                () => throw new ArgumentException()
-            );
+    public Task<IActionResult> Delete(Guid id) =>
+        booksRepository
+            .Lookup(id)
+            .BindAsync(booksRepository.Delete)
+            .MapAsync<Book, IActionResult>(async book =>
+                View(new BooksDeleteViewModel(book, (await booksRepository.All()).Count())))
+            .OrElse(StatusCode(500, "An unexpected error occurred on the server."));
 
     private static Task<Maybe<FormData>> ParseFormData(IFormCollection collection) =>
         Task.FromResult(
-            from title in collection.TryGetValue("title", out var t) ? t.Just() : Nothing
-            from author in collection.TryGetValue("author", out var a) ? a.Just() : Nothing
+            from title in collection.JustGetValue("title")
+            from author in collection.JustGetValue("author")
             select new FormData(title, author)
         );
 }
--- HotwiredBooks/Extensions/FunctionalExtensions.cs
+++ HotwiredBooks/Extensions/FunctionalExtensions.cs
@@ -1,3 +1,4 @@
+using Microsoft.Extensions.Primitives;
 using MonadicBits;
 
 namespace HotwiredBooks.Extensions;
@@ -7,4 +8,19 @@ using static Functional;
 public static class FunctionalExtensions
 {
     public static Maybe<T> ToMaybe<T>(this T value) => value?.Just() ?? Nothing;
+
+    public static Maybe<StringValues> JustGetValue(this IFormCollection collection, string key) =>
+        collection.TryGetValue(key, out var value) ? value.Just() : Nothing;
+
+    public static T OrElse<T>(this Maybe<T> maybe, T orElse) =>
+        maybe.Match(value => value, () => orElse);
+
+    public static T OrElse<T>(this Maybe<T> maybe, Func<T> orElse) =>
+        maybe.Match(value => value, orElse);
+
+    public static async Task<T> OrElse<T>(this Task<Maybe<T>> maybe, Func<T> orElse) =>
+        (await maybe).Match(value => value, orElse);
+
+    public static async Task<T> OrElse<T>(this Task<Maybe<T>> maybe, T orElse) =>
+        (await maybe).Match(value => value, () => orElse);
 }
